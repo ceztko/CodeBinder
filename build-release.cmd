9813bdba29becd43c61d173d92704df689e046c4
Francesco Pretto <ceztko@gmail.com>
2018-12-05 16:35:53 +0100

    Attempt to fix jenkins build issues

--- build-release.cmd
+++ build-release.cmd
@@ -1,4 +1,4 @@
 call "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 nuget restore CodeBinder.sln
-MSBuild CodeBinder.sln /p:Configuration=Release /p:Platform="Any CPU" /t:Build
+MSBuild CodeBinder.sln /p:Configuration=Release /p:Platform="Any CPU" /t:build /restore
 pause
