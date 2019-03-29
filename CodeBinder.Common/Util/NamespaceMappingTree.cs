cd249ea480f79df9000af6ec0c4aa59b13bbcd06
Francesco Pretto <ceztko@gmail.com>
2019-03-30 00:58:52 +0100

    [ADD] Added NamespaceMappingTree

---
+++ CodeBinder.Common/Util/NamespaceMappingTree.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeBinder.Util
+{
+    class NamespaceMappingTree
+    {
+        Node m_root;
+
+        public NamespaceMappingTree() { }
+
+        public void PushMapping(string refns, string mappedns)
+        {
+            var splitted = refns.Split('.');
+            if (m_root == null)
+                m_root = new Node();
+
+            Node node = m_root;
+            for (int i = 0; i < splitted.Length; i++)
+                node = node.GetChildren(splitted[i]);
+
+            node.MappedNamespaceDirect = mappedns;
+        }
+
+        public string GetMappedNamespace(string refns, out string leftoverns)
+        {
+            var splitted = refns.Split('.');
+            if (m_root == null)
+                goto Exit;
+
+            Node node = m_root;
+            int i = 0;
+            for (; i < splitted.Length; i++)
+            {
+                Node found;
+                if (!node.TryGetChildren(splitted[i], out found))
+                    break;
+
+                node = found;
+            }
+
+            if (i != 0)
+            {
+                leftoverns = getSubNamespace(splitted, i);
+                return node.MappedNamespace;
+            }
+
+            Exit:
+            leftoverns = refns;
+            return null;
+        }
+
+        string getSubNamespace(string[] splitted, int index, int npos = -1)
+        {
+            if (npos == 0 || index >= splitted.Length)
+                return null;
+
+            var builder = new StringBuilder();
+            npos = npos < 0 ? splitted.Length : npos;
+            for (int i = index; i < npos; i++)
+            {
+                if (i != index)
+                    builder.Append(".");
+
+                builder.Append(splitted[i]);
+            }
+
+            return builder.ToString();
+        }
+
+        class Node
+        {
+            Dictionary<string, Node> m_children;
+
+            public Node()
+            {
+                Namespace = null;
+                FullNamespace = null;
+            }
+
+            public Node(string parentns, string nspart)
+            {
+                Namespace = nspart;
+                if (parentns == null)
+                    FullNamespace = nspart;
+                else
+                    FullNamespace = parentns + "." + nspart;
+            }
+
+            public bool TryGetChildren(string nspart, out Node node)
+            {
+                if (m_children == null)
+                {
+                    node = null;
+                    return false;
+                }
+
+                return m_children.TryGetValue(nspart, out node);
+            }
+
+            public Node GetChildren(string nspart)
+            {
+                if (m_children == null)
+                    m_children = new Dictionary<string, Node>();
+
+                Node ret;
+                if (!m_children.TryGetValue(nspart, out ret))
+                {
+                    ret = new Node(FullNamespace, nspart);
+                    m_children[nspart] = ret;
+                }
+
+                return ret;
+            }
+
+            public string Namespace { get; private set; }
+            public string FullNamespace { get; private set; }
+            public string MappedNamespaceDirect { get; set; }
+            public string MappedNamespace
+            {
+                get
+                {
+                    if (MappedNamespaceDirect == null)
+                        return FullNamespace;
+
+                    return MappedNamespaceDirect;
+                }
+            }
+        }
+    }
+}
