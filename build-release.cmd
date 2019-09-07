650918c14696324ce8daed8cfd86e555211d11c3
Francesco Pretto <ceztko@gmail.com>
2019-09-07 10:37:19 +0200

    [FIX] Attempt to fix build by removing dependency on nuget.exe

--- build-release.cmd
+++ build-release.cmd
@@ -1,4 +1,3 @@
 call "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-nuget restore CodeBinder.sln || exit /b 1
 MSBuild CodeBinder.sln /p:Configuration=Release /p:Platform="Any CPU" /t:build /restore || exit /b 1
 pause
