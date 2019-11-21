939bfee356d9d2f97e0cd98dd325245696e9980c
Francesco Pretto <ceztko@gmail.com>
2019-11-21 16:51:18 +0100

    [ADD] Added ParameterBinderAttribute, Policies.DotNet

--- CodeBinder.Redist/Attributes.cs
+++ CodeBinder.Redist/Attributes.cs
@@ -5,9 +5,16 @@ using System.Runtime.CompilerServices;
 
 namespace CodeBinder.Attributes
 {
-    [Conditional("CODE_BINDER")]
+    public class CodeBinderAttribute : Attribute
+    {
+        public const string ConditionString = "CODE_BINDER";
+
+        internal CodeBinderAttribute() { }
+    }
+
+    [Conditional(ConditionString)]
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
-    public sealed class ModuleAttribute : Attribute
+    public sealed class ModuleAttribute : CodeBinderAttribute
     {
         public ModuleAttribute(string name)
         {
@@ -17,9 +24,9 @@ namespace CodeBinder.Attributes
         public string Name { get; private set; }
     }
 
-    [Conditional("CODE_BINDER")]
+    [Conditional(ConditionString)]
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
-    public sealed class ImportAttribute : Attribute
+    public sealed class ImportAttribute : CodeBinderAttribute
     {
         public ImportAttribute(string import)
         {
@@ -29,9 +36,9 @@ namespace CodeBinder.Attributes
         public string ImportName { get; private set; }
     }
 
-    [Conditional("CODE_BINDER")]
+    [Conditional(ConditionString)]
     [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
-    public sealed class OrderAttribute : Attribute
+    public sealed class OrderAttribute : CodeBinderAttribute
     {
         public int Order { get; private set; }
 
@@ -41,16 +48,16 @@ namespace CodeBinder.Attributes
         }
     }
 
-    [Conditional("CODE_BINDER")]
+    [Conditional(ConditionString)]
     [AttributeUsage(AttributeTargets.Delegate | AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Field | AttributeTargets.Property)]
-    public sealed class IgnoreAttribute : Attribute
+    public sealed class IgnoreAttribute : CodeBinderAttribute
     {
 
     }
 
-    [Conditional("CODE_BINDER")]
+    [Conditional(ConditionString)]
     [AttributeUsage(AttributeTargets.Delegate | AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Field | AttributeTargets.Property)]
-    public sealed class RequiresAttribute : Attribute
+    public sealed class RequiresAttribute : CodeBinderAttribute
     {
         private string[] _policies;
 
@@ -60,12 +67,29 @@ namespace CodeBinder.Attributes
         }
     }
 
+    /// <summary>
+    /// Inerit this class to create a binder of a parameter/return value type to a C type
+    /// </summary>
+    [Conditional(ConditionString)]
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
+    public class ParameterBinderAttribute : CodeBinderAttribute
+    {
+        protected ParameterBinderAttribute() { }
+    }
+
     public static class Policies
     {
         public const string Delegates = "{0CA8BB17-A589-41A9-A97C-0E1870837AB4}";
         public const string OperatorOverloading = "{B19FE437-C7FE-41A5-841F-D9170CCEEBA1}";
         public const string ReifiedGenerics = "{E3D821B4-562B-4C9F-8753-1E9C6F4D93A1}";
         public const string YieldReturn = "{6D4647AE-CA4C-49F0-8BE6-138512992E21}";
+        /// <summary>
+        /// Represent functionalities tipically present in all .NET base libraries
+        /// </summary>
+        public const string DotNet = "{162BC1C4-BEEA-4E30-A525-E084E528232C}";
+        /// <summary>
+        /// Represent functionalities tipically present only with .NET Framework
+        /// </summary>
         public const string NETFramework = "{5DD7ECEA-0F59-4781-BCA2-1DC916EB3CFC}";
         public const string ValueTypes = "{5DD7ECEA-0F59-4781-BCA2-1DC916EB3CFC}";
         public const string PassByRef = "{5B8D618C-93E8-46F2-85FE-DE28FEE86196}";
