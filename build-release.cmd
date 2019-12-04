72078ca1b1687ae32c07eee0ceae07616423e921
Francesco Pretto <ceztko@gmail.com>
2019-12-04 12:35:52 +0100

    Better find MSBuild

--- build-release.cmd
+++ build-release.cmd
@@ -1,3 +1,10 @@
-call "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-MSBuild CodeBinder.sln /p:Configuration=Release /p:Platform="Any CPU" /t:build /restore || exit /b 1
+@echo off
+setlocal enabledelayedexpansion
+
+set "VSWHERECMD=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+for /f "usebackq tokens=*" %%i in (`"%VSWHERECMD%" -version 16.0 -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+	set "MSBUILDCMD=%%i"
+)
+
+"%MSBUILDCMD%" CodeBinder.sln /p:Configuration=Release /p:Platform="Any CPU" /t:build /restore || exit /b 1
 pause
