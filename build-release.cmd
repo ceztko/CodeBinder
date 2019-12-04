6912ca0651301424f20e45b34c69222a8d19489e
Francesco Pretto <ceztko@gmail.com>
2019-12-04 11:54:50 +0100

    Build script migrated to VS2019

--- build-release.cmd
+++ build-release.cmd
@@ -1,3 +1,3 @@
-call "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 MSBuild CodeBinder.sln /p:Configuration=Release /p:Platform="Any CPU" /t:build /restore || exit /b 1
 pause
