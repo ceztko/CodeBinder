5e5366312d369ef9aa90ac6775386005207e5ccb
Francesco Pretto <ceztko@gmail.com>
2020-03-06 17:13:06 +0100

    [FIX] Add progresses statements

--- CodeBinder.Apple/ObjC/Builders/ObjCBuilderExtensions_Expressions.cs
+++ CodeBinder.Apple/ObjC/Builders/ObjCBuilderExtensions_Expressions.cs
@@ -204,7 +204,6 @@ namespace CodeBinder.Apple
 
             }
 
-
             return builder;
         }
 
--- CodeBinder.Apple/ObjC/Builders/ObjCBuilderExtensions_Statements.cs
+++ CodeBinder.Apple/ObjC/Builders/ObjCBuilderExtensions_Statements.cs
@@ -14,6 +14,67 @@ namespace CodeBinder.Apple
 {
     static partial class ObjCBuilderExtension
     {
+        // Reference: roslyn/src/Compilers/CSharp/Portable/Generated/Syntax.xml.Main.Generated.cs
+        public static CodeBuilder Append(this CodeBuilder builder, StatementSyntax statement, ObjCCompilationContext context)
+        {
+            var kind = statement.Kind();
+            switch (kind)
+            {
+                case SyntaxKind.Block:
+                    return builder.Append((BlockSyntax)statement, context);
+                case SyntaxKind.BreakStatement:
+                    return builder.Append((BreakStatementSyntax)statement, context);
+                case SyntaxKind.ForEachStatement:
+                    return builder.Append((ForEachStatementSyntax)statement, context);
+                case SyntaxKind.ContinueStatement:
+                    return builder.Append((ContinueStatementSyntax)statement, context);
+                case SyntaxKind.DoStatement:
+                    return builder.Append((DoStatementSyntax)statement, context);
+                case SyntaxKind.EmptyStatement:
+                    return builder.Append((EmptyStatementSyntax)statement, context);
+                case SyntaxKind.ExpressionStatement:
+                    return builder.Append((ExpressionStatementSyntax)statement, context);
+                case SyntaxKind.ForStatement:
+                    return builder.Append((ForStatementSyntax)statement, context);
+                case SyntaxKind.IfStatement:
+                    return builder.Append((IfStatementSyntax)statement, context);
+                case SyntaxKind.LocalDeclarationStatement:
+                    return builder.Append((LocalDeclarationStatementSyntax)statement, context);
+                case SyntaxKind.LockStatement:
+                    return builder.Append((LockStatementSyntax)statement, context);
+                case SyntaxKind.ReturnStatement:
+                    return builder.Append((ReturnStatementSyntax)statement, context);
+                case SyntaxKind.SwitchStatement:
+                    return builder.Append((SwitchStatementSyntax)statement, context);
+                case SyntaxKind.ThrowStatement:
+                    return builder.Append((ThrowStatementSyntax)statement, context);
+                case SyntaxKind.TryStatement:
+                    return builder.Append((TryStatementSyntax)statement, context);
+                case SyntaxKind.UsingStatement:
+                    return builder.Append((UsingStatementSyntax)statement, context);
+                case SyntaxKind.WhileStatement:
+                    return builder.Append((WhileStatementSyntax)statement, context);
+                // Unsupported statements
+                case SyntaxKind.CheckedStatement:
+                case SyntaxKind.UnsafeStatement:
+                case SyntaxKind.LabeledStatement:
+                case SyntaxKind.FixedStatement:
+                case SyntaxKind.LocalFunctionStatement:
+                case SyntaxKind.ForEachVariableStatement:
+                // Unsupported yield statements
+                case SyntaxKind.YieldBreakStatement:
+                case SyntaxKind.YieldReturnStatement:
+                // Unsupported goto statements
+                case SyntaxKind.GotoStatement:
+                case SyntaxKind.GotoCaseStatement:
+                case SyntaxKind.GotoDefaultStatement:
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        #region Statements
+
         public static CodeBuilder Append(this CodeBuilder builder, BlockSyntax syntax, ObjCCompilationContext context, bool skipBraces = false)
         {
             if (skipBraces)
@@ -61,8 +122,8 @@ namespace CodeBinder.Apple
 
         public static CodeBuilder Append(this CodeBuilder builder, ForEachStatementSyntax syntax, ObjCCompilationContext context)
         {
-            builder.Append("for").Space().Parenthesized().Append(syntax.Type, context).Space().Append(syntax.Identifier.Text)
-                .Space().Colon().Space().Append(syntax.Expression, context).Close().AppendLine();
+            builder.Append("for").Space().Parenthesized().Append(syntax.Type, ObjCTypeUsageKind.Declaration, context).
+                Space().Append(syntax.Identifier.Text).Space().Append("in").Space().Append(syntax.Expression, context).Close().AppendLine();
             builder.IndentChild().Append(syntax.Statement, context);
             return builder;
         }
@@ -120,7 +181,7 @@ namespace CodeBinder.Apple
         public static CodeBuilder Append(this CodeBuilder builder, LocalDeclarationStatementSyntax syntax, ObjCCompilationContext context)
         {
             if (syntax.IsConst)
-                builder.Append("final").Space();
+                builder.Append("const").Space();
 
             builder.Append(syntax.Declaration, context).SemiColon();
             return builder;
@@ -128,7 +189,8 @@ namespace CodeBinder.Apple
 
         public static CodeBuilder Append(this CodeBuilder builder, LockStatementSyntax syntax, ObjCCompilationContext context)
         {
-            builder.Append("synchronized").Space().Parenthesized().Append(syntax.Expression, context).Close().AppendLine();
+            // https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Multithreading/ThreadSafety/ThreadSafety.html
+            builder.Append("@synchronized").Space().Parenthesized().Append(syntax.Expression, context).Close().AppendLine();
             builder.IndentChild().Append(syntax.Statement, context);
             return builder;
         }
@@ -154,9 +216,22 @@ namespace CodeBinder.Apple
             return builder;
         }
 
+        public static CodeBuilder Append(this CodeBuilder builder, WhileStatementSyntax syntax, ObjCCompilationContext context)
+        {
+            builder.Append("while").Space().Append(syntax.Condition, context).AppendLine()
+                .IndentChild().Append(syntax.Statement, context);
+            return builder;
+        }
+
+
+        #endregion // Statements
+
+        #region Exception Handling
+
+        // https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Exceptions/Tasks/HandlingExceptions.html
         public static CodeBuilder Append(this CodeBuilder builder, ThrowStatementSyntax syntax, ObjCCompilationContext context)
         {
-            builder.Append("throw");
+            builder.Append("@throw");
             if (syntax.Expression != null)
                 builder.Space().Append(syntax.Expression, context);
 
@@ -165,7 +240,7 @@ namespace CodeBinder.Apple
 
         public static CodeBuilder Append(this CodeBuilder builder, TryStatementSyntax syntax, ObjCCompilationContext context)
         {
-            builder.Append("try").AppendLine();
+            builder.Append("@try").AppendLine();
             builder.Append(syntax.Block, context).AppendLine();
             bool first = true;
             foreach (var catchClause in syntax.Catches)
@@ -178,75 +253,49 @@ namespace CodeBinder.Apple
 
         public static CodeBuilder Append(this CodeBuilder builder, UsingStatementSyntax syntax, ObjCCompilationContext context)
         {
-            builder.Append("try").Space().Parenthesized().Append(syntax.Declaration!, context).Close().AppendLine();
+            Debug.Assert(syntax.Expression == null);
+            Debug.Assert(syntax.Declaration != null);
+            builder.Append(syntax.Declaration, context).Close().EndOfStatement();
+            builder.Append("@try");
+            using (builder.Block())
+            {
+                builder.Append(syntax.Statement, context);
+            }
+            builder.Append("@finally");
+            using (builder.Block())
+            {
+                builder.Bracketed().Append(syntax.Declaration.Variables[0].Identifier.Text).Space().Append("dispose").Close();
+            }
+            return builder;
+        }
+
+        public static CodeBuilder Append(this CodeBuilder builder, FinallyClauseSyntax syntax, ObjCCompilationContext context)
+        {
+            builder.Append("@finally").AppendLine().Append(syntax.Block, context);
+            return builder;
+        }
+
+        public static CodeBuilder Append(this CodeBuilder builder, CatchClauseSyntax syntax, ObjCCompilationContext context)
+        {
+            builder.Append("@catch").Space();
+            if (syntax.Declaration == null)
+                builder.Parenthesized().Append("Exception e").Close().AppendLine();
+            else
+                builder.Parenthesized().Append(syntax.Declaration.Type, context).Space().Append(syntax.Declaration.Identifier.Text).Close().AppendLine();
+
+            builder.Append(syntax.Block, context);
+            return builder;
+        }
+
+        #endregion // Exception Handling
+
+        #region Support
+
+        public static CodeBuilder Append(this CodeBuilder builder, ElseClauseSyntax syntax, ObjCCompilationContext context)
+        {
+            builder.Append("else").AppendLine();
             builder.IndentChild().Append(syntax.Statement, context);
             return builder;
-        }
-
-        public static CodeBuilder Append(this CodeBuilder builder, WhileStatementSyntax syntax, ObjCCompilationContext context)
-        {
-            builder.Append("while").Space().Append(syntax.Condition, context).AppendLine()
-                .IndentChild().Append(syntax.Statement, context);
-            return builder;
-        }
-
-        // Reference: roslyn/src/Compilers/CSharp/Portable/Generated/Syntax.xml.Main.Generated.cs
-        public static CodeBuilder Append(this CodeBuilder builder, StatementSyntax statement, ObjCCompilationContext context)
-        {
-            var kind = statement.Kind();
-            switch (kind)
-            {
-                case SyntaxKind.Block:
-                    return builder.Append((BlockSyntax)statement, context);
-                case SyntaxKind.BreakStatement:
-                    return builder.Append((BreakStatementSyntax)statement, context);
-                case SyntaxKind.ForEachStatement:
-                    return builder.Append((ForEachStatementSyntax)statement, context);
-                case SyntaxKind.ContinueStatement:
-                    return builder.Append((ContinueStatementSyntax)statement, context);
-                case SyntaxKind.DoStatement:
-                    return builder.Append((DoStatementSyntax)statement, context);
-                case SyntaxKind.EmptyStatement:
-                    return builder.Append((EmptyStatementSyntax)statement, context);
-                case SyntaxKind.ExpressionStatement:
-                    return builder.Append((ExpressionStatementSyntax)statement, context);
-                case SyntaxKind.ForStatement:
-                    return builder.Append((ForStatementSyntax)statement, context);
-                case SyntaxKind.IfStatement:
-                    return builder.Append((IfStatementSyntax)statement, context);
-                case SyntaxKind.LocalDeclarationStatement:
-                    return builder.Append((LocalDeclarationStatementSyntax)statement, context);
-                case SyntaxKind.LockStatement:
-                    return builder.Append((LockStatementSyntax)statement, context);
-                case SyntaxKind.ReturnStatement:
-                    return builder.Append((ReturnStatementSyntax)statement, context);
-                case SyntaxKind.SwitchStatement:
-                    return builder.Append((SwitchStatementSyntax)statement, context);
-                case SyntaxKind.ThrowStatement:
-                    return builder.Append((ThrowStatementSyntax)statement, context);
-                case SyntaxKind.TryStatement:
-                    return builder.Append((TryStatementSyntax)statement, context);
-                case SyntaxKind.UsingStatement:
-                    return builder.Append((UsingStatementSyntax)statement, context);
-                case SyntaxKind.WhileStatement:
-                    return builder.Append((WhileStatementSyntax)statement, context);
-                // Unsupported statements
-                case SyntaxKind.CheckedStatement:
-                case SyntaxKind.UnsafeStatement:
-                case SyntaxKind.LabeledStatement:
-                case SyntaxKind.FixedStatement:
-                case SyntaxKind.LocalFunctionStatement:
-                case SyntaxKind.ForEachVariableStatement:
-                // Unsupported yield statements
-                case SyntaxKind.YieldBreakStatement:
-                case SyntaxKind.YieldReturnStatement:
-                // Unsupported goto statements
-                case SyntaxKind.GotoStatement:
-                case SyntaxKind.GotoCaseStatement:
-                case SyntaxKind.GotoDefaultStatement:
-                default:
-                    throw new Exception();
-            }
         }
 
         public static CodeBuilder Append(this CodeBuilder builder, VariableDeclarationSyntax syntax, ObjCCompilationContext context)
@@ -267,31 +316,6 @@ namespace CodeBinder.Apple
         public static CodeBuilder Append(this CodeBuilder builder, EqualsValueClauseSyntax syntax, ObjCCompilationContext context)
         {
             builder.Append("=").Space().Append(syntax.Value, context);
-            return builder;
-        }
-
-        public static CodeBuilder Append(this CodeBuilder builder, FinallyClauseSyntax syntax, ObjCCompilationContext context)
-        {
-            builder.Append("finally").AppendLine().Append(syntax.Block, context);
-            return builder;
-        }
-
-        public static CodeBuilder Append(this CodeBuilder builder, CatchClauseSyntax syntax, ObjCCompilationContext context)
-        {
-            builder.Append("catch").Space();
-            if (syntax.Declaration == null)
-                builder.Parenthesized().Append("Exception e").Close().AppendLine();
-            else
-                builder.Parenthesized().Append(syntax.Declaration.Type, context).Space().Append(syntax.Declaration.Identifier.Text).Close().AppendLine();
-
-            builder.Append(syntax.Block, context);
-            return builder;
-        }
-
-        public static CodeBuilder Append(this CodeBuilder builder, ElseClauseSyntax syntax, ObjCCompilationContext context)
-        {
-            builder.Append("else").AppendLine();
-            builder.IndentChild().Append(syntax.Statement, context);
             return builder;
         }
 
@@ -335,5 +359,7 @@ namespace CodeBinder.Apple
             builder.Append(syntax.Value, context);
             return builder;
         }
+
+        #endregion // Support
     }
 }
