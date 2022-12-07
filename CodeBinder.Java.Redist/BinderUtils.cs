907036ecfc9eb443564aa0d4477403d44c18824b
Francesco Pretto <ceztko@gmail.com>
2022-12-07 23:00:39 +0100

    [FIX] Fixed loading of CodeBinder.Java.Redist from CodeBinder

--- CodeBinder.Java.Redist/BinderUtils.cs
+++ CodeBinder.Java.Redist/BinderUtils.cs
@@ -1,18 +1,29 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 
 namespace CodeBinder.Java
 {
     public static class BinderUtils
     {
-#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
-        public static extern IntPtr NewGlobalRef(object obj);
+        public static IntPtr NewGlobalRef(object obj)
+        {
+            _ = obj;
+            return default;
+        }
 
-        public static extern void DeleteGlobalRef(IntPtr globalref);
+        public static void DeleteGlobalRef(IntPtr globalref)
+        {
+            _ = globalref;
+        }
 
-        public static extern IntPtr NewGlobalWeakRef(object obj);
+        public static IntPtr NewGlobalWeakRef(object obj)
+        {
+            _ = obj;
+            return default;
+        }
 
-        public static extern void DeleteGlobalWeakRef(IntPtr globalref);
-#pragma warning restore CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+        public static void DeleteGlobalWeakRef(IntPtr globalref)
+        {
+            _ = globalref;
+        }
     }
 }
