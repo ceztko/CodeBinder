40bf90e88568e82383353d6361b63575b2fab4d9
Francesco Pretto <ceztko@gmail.com>
2023-05-26 17:09:30 +0200

    [FIX] JNI: Fixed BinderClass signatures

--- CodeBinder.Java/JNI/Resources/JNIBinderUtils.cpp
+++ CodeBinder.Java/JNI/Resources/JNIBinderUtils.cpp
@@ -2,37 +2,37 @@
 
 extern "C"
 {
-    JNIEXPORT jlong JNICALL Java_CodeBinder_Java_BinderUtils_newGlobalRef(
+    JNIEXPORT jlong JNICALL Java_CodeBinder_BinderUtils_newGlobalRef(
         JNIEnv *env, jclass, jobject obj)
     {
         return (jlong)env->NewGlobalRef(obj);
     }
 
-    JNIEXPORT void JNICALL Java_CodeBinder_Java_BinderUtils_deleteGlobalRef(
+    JNIEXPORT void JNICALL Java_CodeBinder_BinderUtils_deleteGlobalRef(
         JNIEnv *env, jclass, jlong globalref)
     {
         env->DeleteGlobalRef((jobject)globalref);
     }
 
-	JNIEXPORT jlong JNICALL Java_CodeBinder_Java_BinderUtils_newGlobalWeakRef(
+	JNIEXPORT jlong JNICALL Java_CodeBinder_BinderUtils_newGlobalWeakRef(
 		JNIEnv *env, jclass, jobject obj)
 	{
         return (jlong)env->NewWeakGlobalRef(obj);
 	}
 
-	JNIEXPORT void JNICALL Java_CodeBinder_Java_BinderUtils_deleteGlobalWeakRef(
+	JNIEXPORT void JNICALL Java_CodeBinder_BinderUtils_deleteGlobalWeakRef(
 		JNIEnv *env, jclass, jlong globalref)
 	{
         env->DeleteWeakGlobalRef((jobject)globalref);
 	}
 
-    JNIEXPORT jobject JNICALL Java_CodeBinder_Java_BinderUtils_getGlobalRefTarget(
+    JNIEXPORT jobject JNICALL Java_CodeBinder_BinderUtils_getGlobalRefTarget(
         JNIEnv* env, jclass, jlong globalref)
     {
         return (jobject)globalref;
     }
 
-    JNIEXPORT jobject JNICALL Java_CodeBinder_Java_BinderUtils_getGlobalWeakRefTarget(
+    JNIEXPORT jobject JNICALL Java_CodeBinder_BinderUtils_getGlobalWeakRefTarget(
         JNIEnv* env, jclass, jlong weakRef)
     {
         return (jobject)env->NewLocalRef((jobject)weakRef);
