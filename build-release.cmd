f2abae6db6e19e204d5917d8ba88b939db615d8c
Francesco Pretto <ceztko@gmail.com>
2023-07-31 18:50:52 +0200

    [FIX] Try fix ci

--- build-release.cmd
+++ build-release.cmd
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 set "VSWHERECMD=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
-for /f "usebackq tokens=*" %%i in (`"%VSWHERECMD%" -version 16.0 -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+for /f "usebackq tokens=*" %%i in (`"%VSWHERECMD%" -version 17.0 -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
 	set "MSBUILDCMD=%%i"
 )
 
