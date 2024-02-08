5ee16daa8a5c7ff6d6853e05445b794507e85387
Francesco Pretto <ceztko@gmail.com>
2024-02-08 17:22:22 +0100

    [ADD] TS: Added support for more architectures

--- CodeBinder.JavaScript/TypeScript/TypeScriptNAPIWrapperWriter.cs
+++ CodeBinder.JavaScript/TypeScript/TypeScriptNAPIWrapperWriter.cs
@@ -1,4 +1,6 @@
-﻿namespace CodeBinder.JavaScript.TypeScript;
+﻿using CodeBinder.Attributes;
+
+namespace CodeBinder.JavaScript.TypeScript;
 
 class TypeScriptNAPIWrapperWriter : TypeScriptConversionWriter
 {
@@ -16,13 +18,25 @@ class TypeScriptNAPIWrapperWriter : TypeScriptConversionWriter
     {
         builder.AppendLine("import * as proc from 'node:process';");
         builder.AppendLine($"import * as CodeBinder from './CodeBinder{Context.Conversion.TypeScriptModuleLoadSuffix}';");
-        if (Context.Conversion.GenerationFlags.HasFlag(TypeScriptGenerationFlags.CommonJSCompat))
-            builder.AppendLine("import * as path from 'path';");
-        else
+        builder.AppendLine("import * as path from 'path';");
+        if (!Context.Conversion.GenerationFlags.HasFlag(TypeScriptGenerationFlags.CommonJSCompat))
             builder.AppendLine("import { fileURLToPath } from 'node:url';");
         builder.AppendLine();
 
         builder.AppendLine("""
+const archsMap: { [key: string]: any } = {
+    'win32|x86': 'Win32',
+    'win32|x64': 'Win64',
+    'linux|x64': 'linux-x86_64',
+    'darwin|x64': 'macos-x86_64',
+    'darwin|arm64': 'macos-arm64',
+  };
+
+let narch = `${proc.platform}|${proc.arch}`
+let arch = archsMap[narch];
+if (arch === undefined)
+    throw new Error(`Unsupported architecture ${narch}`);
+
 let shprefix = 'lib';
 let shext = 'so';
 switch (proc.platform)
@@ -40,18 +54,29 @@ switch (proc.platform)
     }
 }
 
-const mod = { exports: {} };
 // https://github.com/DefinitelyTyped/DefinitelyTyped/discussions/65252
+
+const mod = { exports: {} };
 """);
 
-        if (Context.Conversion.GenerationFlags.HasFlag(TypeScriptGenerationFlags.CommonJSCompat))
-            builder.AppendLine($"(proc as any).dlopen(mod, path.join(__dirname, `${{shprefix}}{Context.LibraryName}.${{shext}}`));");
-        else
-            builder.AppendLine($"(proc as any).dlopen(mod, fileURLToPath(new URL(`${{shprefix}}{Context.LibraryName}.${{shext}}`, import.meta.url)));");
+        var dependencies = Context.Compilation.Assembly.GetAttributes<NativeDependencyAttribute>();
+        foreach (var dependency in dependencies)
+            appendLoadLibrary(builder, dependency.GetConstructorArgument<string>(0));
+
+        appendLoadLibrary(builder, Context.LibraryName);
 
         builder.AppendLine($"""
 let napi = (mod.exports as any)({ConversionCSharpToTypeScript.CodeBinderNamespace});
 export default napi;
 """);
+    }
+
+    void appendLoadLibrary(CodeBuilder builder, string libraryName)
+    {
+        if (Context.Conversion.GenerationFlags.HasFlag(TypeScriptGenerationFlags.CommonJSCompat))
+            builder.AppendLine($"(proc as any).dlopen(mod, path.join(__dirname, arch, `${{shprefix}}{libraryName}.${{shext}}`));");
+        else
+            builder.AppendLine($"(proc as any).dlopen(mod, fileURLToPath(new URL(path.join(arch, `${{shprefix}}{libraryName}.${{shext}}`), import.meta.url)));");
+
     }
 }
