3ba30e80c4e10484c683df4dcf5d634d1be1e998
Francesco Pretto <ceztko@gmail.com>
2026-09-17 17:57:36 +0200

    Fix warning in CB_JNIExports both extern and declared in gcc, which makes no sense (it's extern "C", not just extern)

--- CodeBinder.Java/JNI/JNIMethodInitConversion.cs
+++ CodeBinder.Java/JNI/JNIMethodInitConversion.cs
@@ -22,27 +22,32 @@ class JNIMethodInitConversion : ConversionWriter
         builder.AppendLine();
         builder.AppendLine("// Reference this symbol to ensure all functions are defined");
         builder.AppendLine("// See https://github.com/dotnet/samples/tree/3870722f5c5e80fd6a70946e6e96a5c990620e42/core/nativeaot/NativeLibrary#user-content-building-static-libraries");
-        builder.AppendLine("extern \"C\" void* CB_JNIExports[] = {");
+        builder.AppendLine("extern \"C\"");
+        builder.AppendLine("{");
         using (builder.Indent())
         {
-            foreach (var module in _compilation.Modules)
+            builder.AppendLine("void* CB_JNIExports[] = {");
+            using (builder.Indent())
             {
-                foreach (var method in module.Methods)
+                foreach (var module in _compilation.Modules)
                 {
-                    string? condition = null;
-                    if (method.TryGetAttribute<ConditionAttribute>(_compilation, out var attr))
+                    foreach (var method in module.Methods)
                     {
-                        condition = attr.GetConstructorArgument<string>(0);
-                        builder.Append("#ifdef").Space().Append(condition).AppendLine();
+                        string? condition = null;
+                        if (method.TryGetAttribute<ConditionAttribute>(_compilation, out var attr))
+                        {
+                            condition = attr.GetConstructorArgument<string>(0);
+                            builder.Append("#ifdef").Space().Append(condition).AppendLine();
+                        }
+                        builder.Append("(void *)").Append(method.GetJNIMethodName(module)).AppendLine(",");
+                        if (condition != null)
+                            builder.Append("#endif //").Space().Append(condition).AppendLine();
                     }
-                    builder.Append("(void *)").Append(method.GetJNIMethodName(module)).AppendLine(",");
-                    if (condition != null)
-                        builder.Append("#endif //").Space().Append(condition).AppendLine();
                 }
             }
+            builder.Append("}").EndOfLine();
         }
-
-        builder.Append("}").EndOfLine();
+        builder.AppendLine("}");
     }
 
     protected override string GetGeneratedPreamble() => ConversionCSharpToJNI.SourcePreamble;
